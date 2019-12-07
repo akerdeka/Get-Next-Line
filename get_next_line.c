c90509bb415b359023e0e37c6a7fdfa0d31946b6
Antoine Kerdekachian <akerdeka@z2r1p5.le-101.fr>
2019-12-07 14:09:48 +0100

    GNL commit 1

---
+++ get_next_line.c
@@ -0,0 +1,123 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   get_next_line.c                                  .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: akerdeka <marvin@le-101.fr>                +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2019/11/08 10:59:39 by akerdeka     #+#   ##    ##    #+#       */
+/*   Updated: 2019/11/08 11:00:09 by akerdeka    ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+int		ft_read_norme(char *buff, char **str, char *temp)
+{
+	if (!(temp = ft_strdup(*str)))
+		return (-1);
+	free(*str);
+	if (!(*str = ft_strjoin(temp, buff)))
+		return (-1);
+	free(buff);
+	return (1);
+}
+
+int		ft_read(int fd, char **str)
+{
+	int		ret;
+	char	*buff;
+	char	*temp;
+
+	if (!(buff = malloc(sizeof(char) * (BUFFER_SIZE + 1))))
+		return (-1);
+	while ((ret = read(fd, buff, BUFFER_SIZE)))
+	{
+		if (ret < 0)
+			return (-1);
+		buff[ret] = '\0';
+		if (ft_strchr(buff, '\n'))
+		{
+			if (ft_read_norme(buff, str, temp) == 1)
+				return (1);
+			return (-1);
+		}
+		if (!(temp = ft_strdup(*str)))
+			return (-1);
+		free(*str);
+		if (!(*str = ft_strjoin(temp, buff)))
+			return (-1);
+	}
+	free(buff);
+	return (0);
+}
+
+int		ft_fill(int fd, char **str, char **line)
+{
+	int			ret;
+	char		*temp;
+
+	if ((ret = ft_read(fd, str)) == -1)
+		return (-1);
+	if (ret != 0)
+	{
+		if (!(*line = ft_substr(*str, 0, ft_strchr(*str, '\n') - *str)))
+			return (-1);
+		if (!(temp = ft_strdup(ft_strchr(*str, '\n') + 1)))
+			return (-1);
+		free(*str);
+		*str = temp;
+		return (1);
+	}
+	else
+	{
+		if (!(*line = ft_substr(*str, 0, ft_strchr(*str, '\0') - *str)))
+			return (-1);
+		free(*str);
+		*str = NULL;
+		return (0);
+	}
+}
+
+int		get_next_line_norme(char **str, char **line)
+{
+	char	*temp;
+
+	if (!(*line = ft_substr(*str, 0, ft_strchr(*str, '\n') - *str)))
+		return (-1);
+	if (!(temp = ft_strdup(ft_strchr(*str, '\n') + 1)))
+		return (-1);
+	free(*str);
+	*str = temp;
+	return (1);
+}
+
+int		get_next_line(int fd, char **line)
+{
+	static char	*str = NULL;
+	int			ret;
+
+	if (BUFFER_SIZE <= 0 || fd < 0 || !line)
+		return (-1);
+	if (str == NULL)
+		if (!(str = ft_strdup("")))
+			return (-1);
+	if (!(ft_strchr(str, '\n')))
+	{
+		if ((ret = ft_fill(fd, &str, line)) <= 0)
+		{
+			free(str);
+			return (ret == -1 ? -1 : 0);
+		}
+		return (1);
+	}
+	else
+	{
+		if (get_next_line_norme(&str, line) == 1)
+			return (1);
+		return (-1);
+	}
+	free(str);
+	return (0);
+}
---
+++ get_next_line.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   get_next_line.h                                  .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: akerdeka <marvin@le-101.fr>                +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2019/11/08 11:00:36 by akerdeka     #+#   ##    ##    #+#       */
+/*   Updated: 2019/11/08 11:02:21 by akerdeka    ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+
+int		get_next_line(int fd, char **line);
+size_t	ft_strlen(const char *str);
+char	*ft_strdup(const char *s);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strchr(const char *s, int c);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+
+#endif
